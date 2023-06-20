b63407faf15c1fcdd7ea0914b17d6432ef4cd76f
Alfonso Tienda <afoone@hotmail>
2023-06-20 19:46:42 +0200

    model

--- src/main/java/org/alfonso/nextraining/controllers/HolaMundoController.java
+++ src/main/java/org/alfonso/nextraining/controllers/HolaMundoController.java
@@ -2,20 +2,47 @@ package org.alfonso.nextraining.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.alfonso.nextraining.models.Persona;
+import org.alfonso.nextraining.services.PersonaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 class HolaMundoController {
 
+    @Autowired
+    private PersonaService personaService;
+
     @GetMapping("/hola/{nombre}")
     public String holaMundo(@PathVariable String nombre) {
         return "Hola " + nombre;
     }
 
-    @GetMapping("/persona")
-    public Persona getPersona() {
-        return new Persona("Alfonso", 49);
+    // @GetMapping("/personas/")
+    // devuelva toda la lista de personas
+
+    @GetMapping("/persona/{nombre}")
+    public Persona getPersona(@PathVariable String nombre) {
+        System.out.println(personaService.getPersonas().size() + " personas hay en el servicio");
+
+        Persona result = null;
+
+        for (int i = 0; i < personaService.getPersonas().size(); i++) {
+            if (personaService.getPersonas().get(i).getNombre().equals(nombre)) {
+                result = personaService.getPersonas().get(i);
+            }
+        }
+
+        return result;
     }
+
+    @GetMapping("/add/{nombre}/{edad}")
+    public Persona addPersona(@PathVariable String nombre, @PathVariable Integer edad ) {
+        Persona nuevaPersona = new Persona(nombre, edad);
+        // añadir a la persona a la lista
+
+        return nuevaPersona;
+    }
+
 
 }
---
+++ src/main/java/org/alfonso/nextraining/services/PersonaService.java
@@ -0,0 +1,33 @@
+package org.alfonso.nextraining.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.alfonso.nextraining.models.Persona;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonaService {
+
+    private List<Persona> personas;
+
+    public PersonaService() {
+        personas = new ArrayList<>();
+        personas.add(new Persona("pepito", 48));
+        personas.add(new Persona("juanito", 18));
+        personas.add(new Persona("menganito", 17));
+        personas.add(new Persona("fulanito", 16));
+        personas.add(new Persona("juanita", 56));
+        personas.add(new Persona("menganita", 17));
+        personas.add(new Persona("fulanita", 16));
+    }
+
+    // accessors
+    public List<Persona> getPersonas() {
+        return personas;
+    }
+
+    // método que añada una persona a la lista
+
+
+}
