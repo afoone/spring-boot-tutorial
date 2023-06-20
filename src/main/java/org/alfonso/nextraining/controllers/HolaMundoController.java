b1aafb044a50a63a75c98bb6c908664e26eadf76
Alfonso Tienda <afoone@hotmail>
2023-06-20 18:46:40 +0200

    hola

--- src/main/java/org/alfonso/nextraining/controllers/HolaMundoController.java
+++ src/main/java/org/alfonso/nextraining/controllers/HolaMundoController.java
@@ -1,8 +1,14 @@
 package org.alfonso.nextraining.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 class HolaMundoController {
+
+    @GetMapping("/hola")
+    public String holaMundo() {
+        return "Hola Mundo";
+    }
 
 }
